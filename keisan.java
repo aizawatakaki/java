90ce33704717a0e117f1b4564c86410c7be128c4
aizawatakaki <21311001ta@tama.ac.jp>
2014-10-22 15:59:02 +0900

    計算完成

---
+++ keisan.java
@@ -0,0 +1,27 @@
+class keisan {
+	public static void main(String args[])
+	{
+    	// “ü—ÍF‚Q‚Â‚Ì”’l‚ğ“ü—Í
+    	int in0 = new java.util.Scanner(System.in).nextInt();
+    	int in1 = new java.util.Scanner(System.in).nextInt();
+    	System.out.println(in0+","+in1);
+    	
+    	// ˆ—F‘«‚µZ
+    	int add = in0+in1;
+    	// ˆ—Fˆø‚«Z
+    	int sub = in0-in1;
+    	// ˆ—FŠ|‚¯Z
+		int a = in0*in1;
+    	// ˆ—FŠ„‚èZ
+		int b = in0/in1;
+    	// ˆ—F—]‚è
+    	int c = in0%in1;
+    	// ŒvZŒ‹‰Ê‚ğo—Í
+    	System.out.println("‘«‚µZF"+add);
+    	System.out.println("ˆø‚«ZF"+sub);
+		System.out.println(+a);
+		System.out.println(+b);
+		System.out.println(+c);
+	}
+}
+
